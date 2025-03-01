b5e8850dffeee6f310838df7843caf7292973398
gavlna <kopciljakub@gmail.com>
2025-03-01 13:08:49 +0100

    fibonacci reversed - fibonacci fixed to correctly use carry - fibonacci parameters (R, S, M) detector added

--- seedseeker/generators/fibonacci.py
+++ seedseeker/generators/fibonacci.py
@@ -6,16 +6,21 @@ from mod import Mod
 from defs import IntegerRNG, RealRNG
 
 
-def fibonacci(r: int, s: int, seed: list[int], m: int) -> IntegerRNG:
+def fibonacci(
+    r: int, s: int, seed: list[int], m: int, withCarry: bool = True
+) -> IntegerRNG:
     assert len(seed) == max(r, s), f"Seed must be of length max(r, s) ({max(r, s)})"
     queue = deque(Mod(n, m) for n in seed)
-
+    carry = False
     while True:
         value = queue[-r] + queue[-s]
 
-        # Check for "overflow" (in mod m) and add carry
-        if value < queue[-r] or value < queue[-s]:
+        if carry:
             value += 1
+            carry = False
+        # Check for "carry" (in mod m) and add carry
+        if withCarry and (value < queue[-r] or value < queue[-s]):
+            carry = True
 
         yield int(value)
 
@@ -27,10 +32,56 @@ def fibonacci_real(r: int, s: int, seed: list[int], m: int) -> RealRNG:
     yield from (x_n / m for x_n in fibonacci(r, s, seed, m))
 
 
-if __name__ == "__main__":
-    r = 7
-    s = 10
-    seed = [1, 3, 6, 7, 9, 21, 12, 15, 18, 19]
-    m = 32
+def reverse_fibonacci(generator: IntegerRNG, maxParam=1000):
+    """
+    reverses lagged fibonacci PRNG that relies on addion of 2 elements
+    """
+    data = list(islice(generator, maxParam + 100))
+    output = None
+    for r in range(maxParam):
+        for s in range(1, r):
+            assumed_mod = None
+            with_carry = False
+            # if not (r == 607 and s == 334):
+            #    continue
+            for i in range(r, len(data)):
+                new_assumed_mod = data[i - r] + data[i - s] - data[i]
+                if abs(new_assumed_mod) <= 1:
+                    continue
 
-    print(*islice(fibonacci(r, s, seed, m), 100), sep=", ")
+                if assumed_mod is None:
+                    assumed_mod = new_assumed_mod
+                    continue
+                if assumed_mod == new_assumed_mod:
+                    continue
+                if new_assumed_mod - assumed_mod == 1:
+                    assumed_mod = new_assumed_mod
+                    with_carry = True
+                    continue
+                if new_assumed_mod - assumed_mod == -1:
+                    with_carry = True
+                    continue
+
+                break
+            else:
+                print(
+                    f"Found a good match M = {assumed_mod}, R = {r}, S = {s}, with{'' if with_carry else 'out'} carry"
+                )
+                if output is None:
+                    output = assumed_mod, r, s, with_carry
+    return output
+
+
+if __name__ == "__main__":
+    import random
+
+    r = 3217
+    s = 576
+    m = 2**32
+    carry = True
+    seed = [random.randint(0, m) for _ in range(max(s, r))]
+
+    PRNG = fibonacci(r, s, seed, m, carry)
+    print(*islice(PRNG, 10000), sep=", ")
+    print(m, r, s, "with" if carry else "without", "carry")
+    print(reverse_fibonacci(PRNG, 5000), sep=", ")
