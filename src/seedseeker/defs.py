7f77bae3702679c6e96d0e56d963d48e5fe782f8
Maneren <maneren731@gmail.com>
2025-04-26 15:14:52 +0200

    rework IntegerRNG as Protocol

--- src/seedseeker/defs.py
+++ src/seedseeker/defs.py
@@ -1,4 +1,25 @@
 from collections.abc import Iterator
+from typing import Protocol, Self
 
-IntegerRNG = Iterator[int]
+
+class IntegerRNG[StateT](Protocol):
+    """Integer random number generator protocol."""
+
+    def __iter__(self) -> Iterator[int]:
+        """Return the iterator."""
+        return self
+
+    def __next__(self) -> int:
+        """Return the next value."""
+        raise NotImplementedError
+
+    def state(self) -> StateT:
+        """Return the inner state."""
+        raise NotImplementedError
+
+    def from_state(self, state: StateT) -> Self:
+        """Set the inner state."""
+        raise NotImplementedError
+
+
 RealRNG = Iterator[float]
